634843c3086cb357d49426ef48fa002e5550f381
Bruno Ribeiro <brunoribeironasci@gmail.com>
2023-11-13 15:21:44 -0300

    Possíveis movimentos de uma peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on the target position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the choosen piece");
+		}
 	}
 	
 	private void placeNewPiece(char Column, int row, ChessPiece piece) {
